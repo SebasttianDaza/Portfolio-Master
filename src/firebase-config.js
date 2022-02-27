137d4d9c593c908469e32947bd047e1e539d9eb5
SebasttianDaza <jsebastiandaza2002@gmail.com>
2022-02-26 19:51:52 -0500

    CConfig Firebase, ReactFire and Hosting
    I´m doing a config to implement firebase hosting

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyC_PWo2LHGGMfI3EREJcjoAnfG5YEqMyNU",
+  authDomain: "react-portfolio-863d8.firebaseapp.com",
+  projectId: "react-portfolio-863d8",
+  storageBucket: "react-portfolio-863d8.appspot.com",
+  messagingSenderId: "792455783909",
+  appId: "1:792455783909:web:c78e25f8d300cd7c623b0c",
+};
