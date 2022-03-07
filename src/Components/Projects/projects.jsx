77049d2f1411689a7218b6fb6c3af8a89e0b7dca
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-03-07 17:23:16 -0500

    Add render for elements Projects
    I've been working function to render elements and a pagination for the projects

--- src/Components/Projects/projects.jsx
+++ src/Components/Projects/projects.jsx
@@ -1,9 +1,94 @@
-import { Card } from "../Hobbies/hobbies";
+import React from 'react'
 import "./projects.css";
+import { Card } from "../Hobbies/hobbies";
 
 
-const Projects = (props) => {
+class Projects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      elements: [],
+      page: 0,
+      hasError: false
+    };
+    this.getProps = this.getProps.bind(this);
+    this.nextPage = this.nextPage.bind(this);
+    this.prevPage = this.prevPage.bind(this);
+    
+  }
 
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  getProps() {
+    this.setState({
+      elements: this.props.dataProjects.slice(this.state.page, this.state.page + 3),
+    });
+  }
+
+  nextPage () {
+    if (this.state.page < this.props.dataProjects.length) {
+      this.setState({
+        page:  this.state.page + 3
+      });
+      this.getProps();
+    } else {
+      alert("No more projects");
+    }
+  }
+
+  prevPage() {
+    if (this.state.page > 1) {
+      this.setState({
+        page: this.state.page - 3
+      });
+      this.getProps();
+    } else {
+      alert("No more projects");
+    }
+  }
+
+  componentDidMount () {
+    this.getProps();
+  }
+
+
+  render () {
+    console.log(this.state.elements);
+    console.log(this.state.page)
+    const element = this.state.elements.map((element, index) => {
+      return (
+        <Card
+          key={index}
+          clases="projectCard"
+          clasesChild="cardChild"
+          image={element.image}
+          name={element.name}
+          clasesInformation="cardText"
+          description={element.tags}
+          classText="titleCard"
+          classTextSecond="tagsCard"
+          otherElement={
+            <div className="otherElementCard">
+              <p>{element.description}</p>
+              <div className="contenBtn">
+                <button>
+                  <a href={element.demo}>Demo</a>
+                </button>
+                <button>
+                  <a href={element.repo}>Code</a>
+                </button>
+              </div>
+            </div>
+          }
+        ></Card>
+      );
+    });
+
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
     return (
       <footer className="styleProjects">
         <section className="cardStyle">
@@ -20,20 +105,16 @@ const Projects = (props) => {
             </li>
           </ul>
         </section>
-        <section>
-          <Card
-            clases="projectCard"
-            clasesChild="cardChild"
-            image="https://firebasestorage.googleapis.com/v0/b/emprendeyourlifestyle.appspot.com/o/URLShortener.png?alt=media&token=2a97466c-129e-41de-98c6-6c3035ebd30d"
-            name="Checkout"
-            clasesInformation="cardText"
-            description="#HTML #CSS #JS"
-            classText="titleCard"
-            classTextSecond="descriptionCard"
-          ></Card>
+        <section className="cardProjects">{
+          element
+        }</section>
+        <section className="Pagination">
+          <button onClick={this.prevPage}></button>
+          <button onClick={this.nextPage} ></button>
         </section>
       </footer>
     );
+  }
 }
 
 export default Projects;
--- src/main.jsx
+++ src/main.jsx
@@ -17,19 +17,83 @@ import Projects from './Components/Projects/projects';
 
 
 
+
 class Main extends React.Component {
   constructor(props) {
     super(props);
     
   }
 
-
   render() {
+
+    const Data = [
+      {
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/emprendeyourlifestyle.appspot.com/o/challenge.svg?alt=media&token=1d4f8603-0778-4656-a9fb-845bc9f10c21",
+        name: "404 Not Found",
+        tags: "HTML, CSS, JavaScript",
+        description:
+          "Create a 404 not found page following the design. The page should be responsive. Don’t look at the existing solution. Fulfill user stories below:",
+        demo: "https://404-not-found-master-three.vercel.app/",
+        repo: "https://github.com/SebasttianDaza/404-Not-Found-Master",
+      },
+      {
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/emprendeyourlifestyle.appspot.com/o/challenge.svg?alt=media&token=1d4f8603-0778-4656-a9fb-845bc9f10c21",
+        name: "404 Not Found",
+        tags: "HTML, CSS, JavaScript",
+        description:
+          "Create a 404 not found page following the design. The page should be responsive. Don’t look at the existing solution. Fulfill user stories below:",
+        demo: "https://404-not-found-master-three.vercel.app/",
+        repo: "https://github.com/SebasttianDaza/404-Not-Found-Master",
+      },
+      {
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/emprendeyourlifestyle.appspot.com/o/challenge.svg?alt=media&token=1d4f8603-0778-4656-a9fb-845bc9f10c21",
+        name: "404 Not Found",
+        tags: "HTML, CSS, JavaScript",
+        description:
+          "Create a 404 not found page following the design. The page should be responsive. Don’t look at the existing solution. Fulfill user stories below:",
+        demo: "https://404-not-found-master-three.vercel.app/",
+        repo: "https://github.com/SebasttianDaza/404-Not-Found-Master",
+      },
+      {
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/emprendeyourlifestyle.appspot.com/o/challenge.svg?alt=media&token=1d4f8603-0778-4656-a9fb-845bc9f10c21",
+        name: "404 Not Found",
+        tags: "HTML, CSS, JavaScript",
+        description:
+          "Create a 404 not found page following the design. The page should be responsive. Don’t look at the existing solution. Fulfill user stories below:",
+        demo: "https://404-not-found-master-three.vercel.app/",
+        repo: "https://github.com/SebasttianDaza/404-Not-Found-Master",
+      },
+      {
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/emprendeyourlifestyle.appspot.com/o/challenge.svg?alt=media&token=1d4f8603-0778-4656-a9fb-845bc9f10c21",
+        name: "Portfolio Page",
+        tags: "HTML, CSS, JavaScript",
+        description:
+          "Create a 404 not found page following the design. The page should be responsive. Don’t look at the existing solution. Fulfill user stories below:",
+        demo: "https://404-not-found-master-three.vercel.app/",
+        repo: "https://github.com/SebasttianDaza/404-Not-Found-Master",
+      },
+      {
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/emprendeyourlifestyle.appspot.com/o/challenge.svg?alt=media&token=1d4f8603-0778-4656-a9fb-845bc9f10c21",
+        name: "Portfolio Page",
+        tags: "HTML, CSS, JavaScript",
+        description:
+          "Create a 404 not found page following the design. The page should be responsive. Don’t look at the existing solution. Fulfill user stories below:",
+        demo: "https://404-not-found-master-three.vercel.app/",
+        repo: "https://github.com/SebasttianDaza/404-Not-Found-Master",
+      },
+    ];
+
     return (
       <React.StrictMode>
         <Profile></Profile>
         <Content></Content>
-        <Projects></Projects>
+        <Projects dataProjects={Data}></Projects>
       </React.StrictMode>
     );
   }
